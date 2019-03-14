5b317a79d08fe072c899a948f57afb584d241b2f
pasiac <dawidpawlowski16@gmail.com>
2019-03-14 22:19:14 +0100

    Working on search option

--- application.py
+++ application.py
@@ -24,31 +24,43 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-class Book:
-    def __init__(self, isbn, title, author, year):
-        self.isbn = isbn
-        self.title = title
-        self.author = author
-        self.year = year
+# class Book:
+#     def __init__(self, isbn, title, author, year):
+#         self.isbn = isbn
+#         self.title = title
+#         self.author = author
+#         self.year = year
+#
+#     def print_book(self):
+#         print(f"isbn: {self.isbn}")
+#         print(f"title: {self.title}")
+#         print(f"author: {self.author}")
+#         print(f"year: {self.year}")
 
-    def print_book(self):
-        print(f"isbn: {self.isbn}")
-        print(f"title: {self.title}")
-        print(f"author: {self.author}")
-        print(f"year: {self.year}")
 
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == 'GET':
         return render_template("index.html")
+    else:
+        item = request.form.get("item")
+        books = (db.execute("SELECT * FROM books WHERE isbn = :item OR author = :item OR title = :item",
+                            {"item": item}))
+        if books is not None:
+            return render_template("index.html", books=books)
+        else:
+            item = "No such book in the list"
+            return render_template("index.html", message=item)
 
 
-@app.route("/search")
-def search():
-        text = request.form.get("search")
-        books = (db.execute("SELECT * FROM books WHERE isbn = :text OR author = :text OR title = :text",
-                            {"text": text})).fetchall
-        return render_template("index.html", books=books)
+# @app.route("/search")
+# def search(searchtext):
+#     books = (db.execute("SELECT * FROM books WHERE isbn = :text OR author = :text OR title = :text",
+#                         {"text": searchtext})).fetchall
+#     if books is not None:
+#         return render_template("index.html", books=books)
+#     else:
+#         return render_template("index.html")
 
 
 # TODO: something with session to get user real log in system
